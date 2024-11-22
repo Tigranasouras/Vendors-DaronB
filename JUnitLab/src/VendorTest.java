45b2d5ceafe8ac18ea4eb88ceadc83e8179898e4
Daron <daronbaltazar@gmail.com>
2024-11-21 23:22:05 -0600

    Added Vendor Test class & Shells for tests
    added the shell for
    1.Set up a JUnit test case to validate that you can add money to a vendor.
    2.Set up a JUnit test to validate that you can buy an item from a vendor.
    3.Set up a JUnit test to validate that you can empty the vendor’s inventory

---
+++ JUnitLab/src/VendorTest.java
@@ -0,0 +1,24 @@
+/*Daron Baltazar
+ * 11/7/24
+ * Backend File for running tests
+ */
+import org.junit.jupiter.api.Test;
+
+public class VendorTest{
+    @Test
+    void addMoneyTest(){
+
+    }
+
+    @Test
+    void buyItemTest(){
+
+    }
+
+    @Test
+    void emptyVendorInventoryTest(){
+
+    }
+
+
+}
