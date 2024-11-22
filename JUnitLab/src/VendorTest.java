11575ca03bc59aa43565b4c8a7d91f21e7822442
Daron <daronbaltazar@gmail.com>
2024-11-21 23:28:03 -0600

    Test for addMoney
    test for adding money to make sure you can add money to the vendor.

--- JUnitLab/src/VendorTest.java
+++ JUnitLab/src/VendorTest.java
@@ -4,10 +4,14 @@
  */
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class VendorTest{
     @Test
     void addMoneyTest(){
-
+    Vending bob = new Vending(5, 3);
+    bob.addMoney(5);
+    assertEquals(5, bob.getBalance(), "Can add Money to vending.");
     }
 
     @Test
